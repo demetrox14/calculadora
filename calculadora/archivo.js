264c28cfadd74ce1970785512d67960799c2ea85
Nirvan <nirvanyamil@gmail.com>
2018-01-12 18:08:08 -0800

    agregue botones de borrar

--- calculadora/archivo.js
+++ calculadora/archivo.js
@@ -46,6 +46,21 @@ function potencia(){
     alert('Proximamente estarán disponibles las potencias, esten atentos')
 }
 
+function borrar_num1(){
+    document.getElementById('num1').value = "";
+
+
+}
+function borrar_num2(){
+    document.getElementById('num2').value = "";
+}
+
+function borrar_todo(){
+    document.getElementById('num1').value = "";
+    document.getElementById('num2').value = "";
+    document.getElementById('res').value = "";
+}
+
     window.onload = function(){
 
         document.getElementById('suma').onclick = suma;
@@ -54,4 +69,7 @@ function potencia(){
         document.getElementById('division').onclick = division;
         document.getElementById('raiz').onclick = raiz;
         document.getElementById('potencia').onclick = potencia;
+        document.getElementById('b_num1').onclick = borrar_num1;
+        document.getElementById('b_num2').onclick = borrar_num2;
+        document.getElementById('b_todo').onclick = borrar_todo;
     }
